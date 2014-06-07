76d77830660c89350f5fa9d014bd3c81503357c9
ajikcey <ajikcey@mail.ru>
2014-06-07 17:58:04 +0400

    v2

--- jsubmitform/jsubmitform.js
+++ jsubmitform/jsubmitform.js
@@ -6,22 +6,26 @@
  animate         - show loading animate (default = true)
  handle          - handle request (default = false)
  auto            - automatic sending request when the field changes (default = false)
+ validate        - array validate: "field", "required", "msg", <function> (order is not important)
 
  Functions:
- before({form, dataout});       - if return false execution stops
- success({form, dataout, datain});
- noresponse({form, dataout});
- after({form, dataout, datain});
+ before({ form, dataout });                 - if return "false" execution stops
+ noresponse({ form, dataout });             - no answer from server
+ after({ form, dataout });
+ success({ form, dataout, datain });
+ success_field({ form, dataout, field });
+ error_field({ form, dataout, field });
 
  Handle request functions:
- incorrect_data({form, dataout, datain});
- correct_data({form, dataout, datain});
- error_data({form});
+ incorrect_data({ form, dataout, datain });   - if JSON answer not consist "success" or "success == false"
+ -                                              if JSON answer consist "field" and no "success", call "error_field"
+ correct_data({ form, dataout, datain });
+ error_data({ form, dataout, datain });
 
  Elements:
- #jsf_error      - element for error (alert() if there is no)
- #jsf_luck       - element for success (alert() if there is no)
- #jsf_animate    - element for loading animate (alert() if there is no)
+ #jsf_error      - element for error/debug info (alert() if there is no)
+ #jsf_luck       - element for success info (alert() if there is no)
+ #jsf_animate    - element for loading animate
 
  Attributes:
  jsf_necessary   - required fields and value
@@ -61,24 +65,12 @@ function jsubmitform_once(form, params) {
             hideel(form.find('#jsf_luck'));
         }
 
-        var err_b = false;
-        form.find('*[jsf_necessary]').each(function () {
+        if (necessary() === false || validate() === false) return false;
 
-            // if not filled in the required fields
-            if ((( $(this).prop('type') == "radio" || $(this).prop('type') == "checkbox" ) &&
-                ( form.find('[name="' + $(this).prop('name') + '"]:checked').length < 1 )) ||
-                ( !$(this).val() )) {
-
-                // error
-                showerr(form.find('#jsf_error'), $(this).attr("jsf_necessary"));
-                err_b = true;
-                return false;
-            }
-        });
-        if (err_b) return false;
+        // !!! dataout !!!
+        var dataout = form.serializeObject();
 
         // before
-        var dataout = form.serializeObject();
         if (callf(params.before, {form: form, dataout: dataout}) == false) return false;
 
         // *befor desabled form necessary save data,
@@ -97,7 +89,6 @@ function jsubmitform_once(form, params) {
             url: form.attr('action'),
             data: serialize,
             success: function (data) {
-
 
                 if (params.handle) {
                     // handle request
@@ -136,7 +127,7 @@ function jsubmitform_once(form, params) {
     function jsf_handle_request(obj) {
         try {
             var o_data = $.parseJSON(obj.datain);
-            if (o_data.success) {
+            if (typeof(o_data.success) != 'undefined' && o_data.success != false) {
                 // correct_data
                 callf(params.correct_data, {form: form, dataout: obj.dataout, datain: o_data});
 
@@ -145,11 +136,18 @@ function jsubmitform_once(form, params) {
                 // incorrect_data
                 callf(params.incorrect_data, {form: form, dataout: obj.dataout, datain: o_data});
 
+                // error_field
+                if (o_data.field) {
+                    callf(params.error_field, {
+                        form: form, dataout: obj.dataout, field: form.find('[name="' + o_data.field + '"]')
+                    });
+                }
+
                 showerr(form.find('#jsf_error'), o_data.error);
             }
         } catch (e) {
             // error_data
-            callf(params.error_data, {form: form, dataout: obj.dataout});
+            callf(params.error_data, {form: form, dataout: obj.dataout, datain: o_data});
 
             // debug
             showdeb(form.find('#jsf_error'), 'debug - incorrect data');
@@ -179,7 +177,7 @@ function jsubmitform_once(form, params) {
 
     // show error
     function showerr(where, what) {
-        if (params.error) {
+        if (params.error === true) {
             if (where.length > 0) {
                 showel(where, what);
             } else {
@@ -191,7 +189,7 @@ function jsubmitform_once(form, params) {
 
     // show luck
     function showlck(where, what) {
-        if (params.luck) {
+        if (params.luck === true) {
             if (where.length > 0) {
                 showel(where, what);
             } else {
@@ -203,7 +201,7 @@ function jsubmitform_once(form, params) {
 
     // show debug
     function showdeb(where, what) {
-        if (params.debug) {
+        if (params.debug === true) {
             if (where.length > 0) {
                 showel(where, what);
             } else {
@@ -225,7 +223,7 @@ function jsubmitform_once(form, params) {
     }
 
 
-    // validation & default
+    // default
     function init() {
         // validation
         if (!form || typeof(form) != "object") {
@@ -255,7 +253,9 @@ function jsubmitform_once(form, params) {
         }
     }
 
+
     /*
+     Description:   FORM TO OBJECT
      Source:    http://css-tricks.com/snippets/jquery/serialize-form-to-json/
      Using:     $('form').serializeObject();
      Return:    (object) { name: 'Alex', gender: 'm' }
@@ -275,4 +275,150 @@ function jsubmitform_once(form, params) {
         });
         return o;
     };
+
+
+    // necessary fields
+    function necessary() {
+        var dataout = form.serializeObject();
+
+        var err_b = false;
+        form.find('[jsf_necessary]').each(function () {
+
+            // if not filled in the required fields, or not valid
+            if ((( $(this).prop('type') == "radio" || $(this).prop('type') == "checkbox" ) &&
+                ( form.find('[name="' + $(this).prop('name') + '"]:checked').length < 1 )) ||
+                ( !$(this).val() )) {
+
+                // error
+                showerr(form.find('#jsf_error'), $(this).attr("jsf_necessary"));
+
+                callf(params.error_field, {form: form, dataout: dataout, field: $(this)});
+
+                err_b = true;
+                return false;
+            }
+
+            callf(params.success_field, {form: form, dataout: dataout, field: $(this)});
+        });
+        if (err_b) return false;
+    }
+
+
+    // validate fields
+    function validate() {
+        var dataout = form.serializeObject();
+
+        var err_b = false;
+        if (typeof(params.validate) === 'object') {
+            /*
+             index - name field
+             value - object: field, required, msg and <function>
+             */
+            $.each(params.validate, function (index, value) {
+                if (typeof(value) === 'object') {
+
+                    // if required OR val
+                    if (value.required == true || form.find('[name="' + value.field + '"]').val()) {
+
+                        if (Object.keys(value).length > 3) {
+                            /*
+                             v_index - field, required, msg or <function>
+                             v_value - value this index
+                             */
+                            $.each(value, function (v_index, v_value) {
+
+                                if (v_index != 'msg' && v_index != 'required' && v_index != 'field') {
+
+                                    if (valid_actions[v_index]({
+                                        field: form.find('[name="' + value.field + '"]'), value: v_value
+                                    }) === false) {
+
+                                        // error
+                                        showerr(form.find('#jsf_error'), value.msg);
+
+                                        callf(params.error_field, {
+                                            form: form, dataout: dataout, field: form.find('[name="' + value.field + '"]')
+                                        });
+
+                                        err_b = true;
+                                        return false;
+                                    }
+                                }
+
+                            });
+                            if (err_b) return false;
+
+                        } else if (!form.find('[name="' + value.field + '"]').val()) {
+                            // error
+                            showerr(form.find('#jsf_error'), value.msg);
+
+                            callf(params.error_field, {
+                                form: form, dataout: dataout, field: form.find('[name="' + value.field + '"]')
+                            });
+
+                            err_b = true;
+                            return false;
+                        }
+                    }
+                }
+            });
+        }
+        if (err_b) return false;
+    }
+
+
+    // !!! validation functions !!!
+
+    var valid_actions = {
+        minlength: function (obj) {
+            var expr = new RegExp('^\\S{' + obj.value + ',}$', 'ig');
+
+            var str = obj.field.val();
+            if (str.search(expr) == -1) return false;
+        },
+        maxlength: function (obj) {
+            var expr = new RegExp('^\\S{1,' + obj.value + '}$', 'ig');
+
+            var str = obj.field.val();
+            if (str.search(expr) == -1) return false;
+        },
+    };
+
+    function debug(obj) {
+        console.log('!!! DEBUG !!!');
+        console.log(obj);
+    }
 }
+
+//// !!! validation functions !!!
+//
+//// validation phone 9 (999) 999-99-99
+//function jsf_valid_phone(val, error) {
+//    if (val.search(/\d \(\d{3}\) \d{3}-\d{2}-\d{2}$/) == -1) {
+//        // error
+//        showerr(form.find('#jsf_error'), error);
+//        return false;
+//    }
+//}
+//
+
+
+// Source: http://habrahabr.ru/post/27170/
+//
+//var actions = {
+//    someFunc: function(params) {
+//        ...
+//    },
+//    goodFunc: function(params) {
+//        ...
+//    }
+//};
+//
+//// это название функции допустим переданное в ответе на AJAX-запрос
+//var funcName = 'someFunc';
+//
+//// а это параметры функции переданные все в том же ответе
+//var funcParams = {title: "Test Title", content: "Test Content"}
+//
+//// вызываем функцию
+//actions[funcName](funcParams);
---
+++ jsubmitform/respond.php
@@ -0,0 +1,3 @@
+<?php
+
+echo json_encode(array('field' => 'text', 'error' => "not true"));
