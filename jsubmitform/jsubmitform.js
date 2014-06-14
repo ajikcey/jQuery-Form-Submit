d17e838cfa213183979a588c46d1a30945d6d645
ajikcey <ajikcey@mail.ru>
2014-06-15 03:18:37 +0400

    all errors and success

--- jsubmitform/jsubmitform.js
+++ jsubmitform/jsubmitform.js
@@ -1,34 +1,31 @@
 /*
  Parameters:
- error           - show error info (default = true) (if not find #jsf_error in form then alert())
- luck            - show success info (default = true) (if not find #jsf_luck in form then alert())
- handle          - handle request (default = false)
- auto            - automatic sending request when the field changes (default = false)
- validate        - objects: "field": "array functions"
+ showerror          - show error info (default = true) (if not find #jsf_error in form then alert())
+ showsuccess        - show success info (default = true) (if not find #jsf_success in form then alert())
+ handle             - handle request (default = false)
+ auto               - automatic sending request when the field changes (default = false)
+ validate           - objects: "field": "array functions"
 
  Functions:
  before({ form, dataout });                 - if return "false" execution stops
  after({ form, dataout });
  success({ form, dataout, datain });
- success_field({ form, dataout, field });
- error_field({ form, dataout, field });
-
- Handle request functions:
- incorrect_data({ form, dataout, datain });   - if JSON answer not consist "success" or "success == false"
- -                                              if JSON answer consist "field" and no "success", call "error_field"
- correct_data({ form, dataout, datain });
+ error({ form, dataout, success_fields, error_fields });
 
  Elements:
  #jsf_error      - element for error info (alert() if there is no)
- #jsf_luck       - element for success info (alert() if there is no)
+ #jsf_success       - element for success info (alert() if there is no)
 
  Attributes:
  jsf_necessary   - required fields and value
  */
 
+// console.log('debug');
 
 // entry point for several forms
 function jsubmitform(forms, params) {
+    var errors = "no response from server;incorrect data".split(";");
+
 
     forms.each(function () {
         jsubmitform_once($(this), params);
@@ -51,12 +48,9 @@ function jsubmitform(forms, params) {
 
         // !!!!!!! MAIN HANDLE !!!!!!!
         function jsf_main_handle() {
-            if (params.error && form.find('#jsf_error')) {
-                hideel(form.find('#jsf_error'));
-            }
-            if (params.luck && form.find('#jsf_luck')) {
-                hideel(form.find('#jsf_luck'));
-            }
+            if (params.showerror && form.find('#jsf_error')) hideel(form.find('#jsf_error'));
+
+            if (params.showsuccess && form.find('#jsf_success')) hideel(form.find('#jsf_success'));
 
             // !!! dataout !!!
             var dataout = form.serializeObject();
@@ -88,14 +82,13 @@ function jsubmitform(forms, params) {
                         // success
                         callf(params.success, {form: form, dataout: dataout, datain: data});
                         // show success data
-                        showlck(form.find('#jsf_luck'), data);
+                        showscc(form.find('#jsf_success'), data);
                     }
 
                 },
                 error: function () {
                     // debug
-                    // console.log('jsubmitform - no response from server');
-                    showerr(form.find('#jsf_error'), 'no response from server');
+                    showerr(form.find('#jsf_error'), errors[0]);
                 }
             }).always(function () {
                 // after
@@ -111,100 +104,135 @@ function jsubmitform(forms, params) {
 
         // necessary fields
         function necessary(dataout) {
-
+            var suc = [];
+            var err = [];
             var err_b = false;
+
             form.find('[jsf_necessary]').each(function () {
 
                 // if not filled in the required fields, or not valid
                 if (!dataout[$(this).prop('name')]) {
-
-                    // error
-                    showerr(form.find('#jsf_error'), $(this).attr("jsf_necessary"));
-
-                    callf(params.error_field, {form: form, dataout: dataout, field: $(this)});
-
                     err_b = true;
-                    return false;
+                    err.push({field: $(this), msg: $(this).attr("jsf_necessary")});
+                } else {
+                    suc.push({field: $(this)});
                 }
-
-                callf(params.success_field, {form: form, dataout: dataout, field: $(this)});
             });
+
+            if (err[0]) {
+                showerr(form.find('#jsf_error'), err[0].msg);
+            }
+
+            callf(params.error, {
+                form: form,
+                dataout: dataout,
+                error_fields: err,
+                success_fields: suc
+            });
+
             if (err_b) return false;
         }
 
 
         // validate fields
         function validate(dataout) {
+            var suc = [];
+            var err = [];
             var err_b = false;
 
             if (typeof(params.validate) === 'object') {
-                /*
-                 value - object: field: <functions>
-                 */
+
+                // value - object: field: <functions>
                 $.each(params.validate, function (index, value) {
                     if (typeof(value) === 'object') {
 
-                        /*
-                         f_value - object: name(function), arg, msg
-                         */
+                        // foreach functions per field
+                        var err_c = false;
                         $.each(value, function (f_index, f_value) {
                             if (typeof(f_value) === 'object') {
 
-                                // call validation function
+                                // call validation function (name function)
                                 if (valid_actions[f_value.name]({
-                                    value: dataout[index], test: f_value.arg
-                                }) === false) {
+                                    value: dataout[index], test: f_value.arg}) === false) {
 
-                                    // error
-                                    showerr(form.find('#jsf_error'), f_value.msg);
 
-                                    callf(params.error_field, {
-                                        form: form,
-                                        dataout: dataout,
-                                        field: form.find('[name="' + index + '"]')
+                                    err.push({
+                                        field: form.find('[name="' + index + '"]'),
+                                        msg: f_value.msg
                                     });
 
                                     err_b = true;
+                                    err_c = true;
                                     return false;
                                 }
                             }
                         });
-                        if (err_b) return false;
-
-                        callf(params.success_field, {
-                            form: form, dataout: dataout, field: form.find('[name="' + index + '"]')
-                        });
+                        if (!err_c) suc.push({field: form.find('[name="' + index + '"]')});
                     }
                 });
             }
+
+            if (err[0]) {
+                showerr(form.find('#jsf_error'), err[0].msg);
+            }
+
+            callf(params.error, {
+                form: form,
+                dataout: dataout,
+                error_fields: err,
+                success_fields: suc
+            });
+
             if (err_b) return false;
         }
 
 
         // handle request
         function jsf_handle_request(obj) {
+            var suc = [];
+            var err = [];
+
             try {
                 var o_data = $.parseJSON(obj.datain);
-                if (typeof(o_data.success) != 'undefined' && o_data.success != false) {
-                    // correct_data
-                    callf(params.correct_data, {form: form, dataout: obj.dataout, datain: o_data});
 
-                    showlck(form.find('#jsf_luck'), o_data.success);
-                } else {
-                    // incorrect_data
-                    callf(params.incorrect_data, {form: form, dataout: obj.dataout, datain: o_data});
-
-                    // error_field
-                    if (o_data.field) {
-                        callf(params.error_field, {
-                            form: form, dataout: obj.dataout, field: form.find('[name="' + o_data.field + '"]')
+                if (o_data.error_fields) {
+                    $.each(o_data.error_fields, function (index, value) {
+                        err.push({
+                            field: form.find('[name="' + index + '"]'),
+                            msg: value
                         });
-                    }
-
-                    showerr(form.find('#jsf_error'), o_data.error);
+                    });
                 }
+
+                if (o_data.success_fields) {
+                    $.each(o_data.success_fields, function (index, value) {
+                        suc.push({
+                            field: form.find('[name="' + value + '"]')
+                        });
+                    });
+                }
+
+                // error_fields
+                callf(params.error, {
+                    form: form,
+                    dataout: obj.dataout,
+                    error_fields: err,
+                    success_fields: suc
+                });
+
+                if (o_data.success) {
+
+                    // success
+                    callf(params.success, {form: form, dataout: obj.dataout, datain: o_data});
+
+                    if (o_data.msg) showscc(form.find('#jsf_success'), o_data.msg);
+                } else {
+                    if (err[0]) showerr(form.find('#jsf_error'), err[0].msg);
+                }
+
+
             } catch (e) {
-                showerr(form.find('#jsf_error'), 'incorrect data');
+                showerr(form.find('#jsf_error'), errors[1]);
             }
         }
 
@@ -220,24 +248,22 @@ function jsubmitform(forms, params) {
 
         // show error
         function showerr(where, what) {
-            if (params.error === true) {
-                if (where.length > 0) {
+            if (params.showerror === true) {
+                if (where.length > 0)
                     showel(where, what);
-                } else {
+                else
                     alert(what);
-                }
             }
         }
 
 
-        // show luck
-        function showlck(where, what) {
-            if (params.luck === true) {
-                if (where.length > 0) {
+        // show success
+        function showscc(where, what) {
+            if (params.showsuccess === true) {
+                if (where.length > 0)
                     showel(where, what);
-                } else {
+                else
                     alert(what);
-                }
             }
         }
 
@@ -257,28 +283,21 @@ function jsubmitform(forms, params) {
         // default
         function init() {
             // validation
-            if (!form || typeof(form) != "object") {
+            if (!form || typeof(form) != "object")
                 return false;
-            }
             // default
-            if (!params || typeof(params) != "object") {
+            if (!params || typeof(params) != "object")
                 params = {};
-            }
-            if (typeof(params.error) != "boolean") {
-                params.error = true;
-            }
-            if (typeof(params.luck) != "boolean") {
-                params.luck = true;
-            }
-            if (typeof(params.handle) != "boolean") {
+            if (typeof(params.showerror) != "boolean")
+                params.showerror = true;
+            if (typeof(params.showsuccess) != "boolean")
+                params.showsuccess = true;
+            if (typeof(params.handle) != "boolean")
                 params.handle = false;
-            }
-            if (typeof(params.auto) != "boolean") {
+            if (typeof(params.auto) != "boolean")
                 params.auto = false;
-            }
-            if (typeof(params.validate) != "object") {
+            if (typeof(params.validate) != "object")
                 params.validate = [];
-            }
         }
 
 
@@ -308,43 +327,45 @@ function jsubmitform(forms, params) {
 
         // !!! validation functions !!!
         var valid_actions = {
-
             minlength: function (obj) {
-                var expr = new RegExp('^\\S{' + obj.test + ',}$', 'ig');
-
-                var str = obj.value;
-                if (str.search(expr) == -1) return false;
+                if (obj.test && obj.value) {
+                    if (obj.value.length < obj.test) return false;
+                } else {
+                    return false;
+                }
             },
-
             maxlength: function (obj) {
-                var expr = new RegExp('^\\S{1,' + obj.test + '}$', 'ig');
-
-                var str = obj.value;
-                if (str.search(expr) == -1) return false;
+                if (obj.test && obj.value) {
+                    if (obj.value.length > obj.test) return false;
+                } else {
+                    return false;
+                }
             },
-
             regexp: function (obj) {
-                var expr = obj.test;
-
-                var str = obj.value;
-                if (str.search(expr) == -1) return false;
+                if (obj.test && obj.value && typeof(obj.value) == "string") {
+                    if (obj.value.search(obj.test) == -1) return false;
+                } else {
+                    return false;
+                }
             },
-
             phone: function (obj) {
-                var expr = /^\d{1,12}\s?(\(\d{1,5}\))?\s?\d{0,5}(-)?\d{0,5}(-)?\d{0,5}$/;
+                if (obj.value && typeof(obj.value) == "string") {
+                    var expr = /^\d{1,12}\s?(\(\d{1,5}\))?\s?\d{0,5}(-)?\d{0,5}(-)?\d{0,5}$/;
 
-                var str = obj.value;
-                if (str.search(expr) == -1) return false;
+                    if (obj.value.search(expr) == -1) return false;
+                } else {
+                    return false;
+                }
             },
-
-
             email: function (obj) {
-                var expr = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\.[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+$/;
+                if (obj.value && typeof(obj.value) == "string") {
+                    var expr = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\.[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+$/;
 
-                var str = obj.value;
-                if (str.search(expr) == -1) return false;
+                    if (obj.value.search(expr) == -1) return false;
+                } else {
+                    return false;
+                }
             },
-
         }
     }
 }
--- jsubmitform/respond.php
+++ jsubmitform/respond.php
@@ -1,3 +1,7 @@
 <?php
 
-echo json_encode(array('success' => true, 'field' => 'text', 'error' => "not true"));
+if (!$_POST['textarea']) {
+	echo json_encode(array('error_fields' => array('textarea' => "not true")));
+} else {
+	echo json_encode(array('success' => true, 'success_fields' => array('textarea'), 'msg' => true));
+}
